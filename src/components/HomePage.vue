41d881d1b8d51d6f80b64b9dd54f070f990e1ea0
Irvayne <irvayne@infoway-pi.com.br>
2024-01-27 15:40:54 -0300

    criando componente para representar o home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <MenuLateral />
+    <div class="conteudo-com-menu">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+
+export default {
+  name: "App",
+  components: {
+    MenuLateral
+  }
+};
+</script>
+
+<style>
+
+#app {
+  display: flex;
+  height: 100vh;
+}
+
+.conteudo-com-menu {
+  flex: 1;
+  overflow: auto; 
+  padding: 20px; 
+  margin-left: 200px; 
+  width: 1200px; 
+  z-index: 1;
+  position: relative;
+}
+
+</style>
--- src/components/MenuLateral.vue
+++ src/components/MenuLateral.vue
@@ -7,37 +7,31 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "MenuLateral"
-};
-</script>
-
 <style scoped>
 .menu-lateral {
   position: fixed;
   top: 0;
   left: 0;
   height: 100%;
-  width: 200px; /* Largura do menu lateral, ajuste conforme necessário */
-  background-color: #f8f9fa; /* Cor de fundo do Bootstrap */
+  width: 200px; 
+  background-color: #f8f9fa; 
   padding-top: 1rem;
-  box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1); /* Sombra lateral para um efeito visual */
+  box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1); 
 }
 
 .logo-img {
-  width: 100px; /* Largura da imagem */
-  margin-left: 40px; /* Margem à esquerda da imagem */
-  margin-bottom: 1rem; /* Adicione margem inferior para separar a imagem dos links */
+  width: 100px;
+  margin-left: 40px; 
+  margin-bottom: 1rem; 
 }
 
 .nav-link {
   padding: 0.5rem 1rem;
   text-decoration: none;
-  color: #495057; /* Cor do texto do Bootstrap */
+  color: #495057; 
 }
 
 .nav-link:hover {
-  background-color: #e9ecef; /* Cor de fundo ao passar o mouse do Bootstrap */
+  background-color: #e9ecef; 
 }
 </style>
